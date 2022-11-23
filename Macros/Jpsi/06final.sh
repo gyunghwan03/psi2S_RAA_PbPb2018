77c4eb695178b20e75ce7fb77be5a55d283ffb97
gyeonghwan <gyunghwan03@gmail.com>
2022-11-23 19:55:05 +0100

    up

--- Macros/Jpsi/06final.sh
+++ Macros/Jpsi/06final.sh
@@ -13,3 +13,12 @@ for pt in  '6.5,9' '9,12' '12,15' '15,20' '20,50'
 do
 	root -l -b -q 'Final2DFit.C('$pt',0,2.4,0,180)'
 done
+
+for cent in '0,20' '20,40' '40,60' '60,80' '80,100' '100,180'
+do
+    root -l -b -q 'Final2DFit.C(6.5,50,0,2.4,'$cent')'
+done
+for cent in '0,40' '40,80' '80,180'
+do
+    root -l -b -q 'Final2DFit.C(3,50,1.6,2.4,'$cent')'
+done
