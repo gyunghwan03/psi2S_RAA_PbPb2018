08ebab6505c4fb64d1f6cb69294a052e739e46fe
pjgwak <piljun.gwak@cern.com>
2024-06-27 10:16:30 +0900

    pp 2S updates

--- Macros/pp_psi2S_230512/systematic/bkgPDF/pjgwak_mass_script.py
+++ Macros/pp_psi2S_230512/systematic/bkgPDF/pjgwak_mass_script.py
@@ -1,40 +1,28 @@
 import subprocess
 
 
-subprocess.call("root -l -b -q MassFit_FixPar_Data.C'(3.5, 6.5, 1.6, 2.4)'", shell=True)
-subprocess.call("root -l -b -q MassFit_FixPar_Data.C'(6.5, 9, 1.6, 2.4)'", shell=True)
-subprocess.call("root -l -b -q MassFit_FixPar_Data.C'(9, 12, 1.6, 2.4)'", shell=True)
-subprocess.call("root -l -b -q MassFit_FixPar_Data.C'(12, 50, 1.6, 2.4)'", shell=True)
-
-subprocess.call("root -l -b -q MassFit_FixPar_Data.C'(3.5, 50, 1.6, 2.4)'", shell=True)
+#subprocess.call("root -l MassFit_FixPar_Data.C'(3.5, 6.5, 1.6, 2.4)'", shell=True)
+#subprocess.call("root -l MassFit_FixPar_Data.C'(6.5, 9, 1.6, 2.4)'", shell=True)
+subprocess.call("root -l MassFit_FixPar_Data.C'(12, 50, 1.6, 2.4)'", shell=True)
+subprocess.call("root -l MassFit_FixPar_Data.C'(3.5, 50, 1.6, 2.4)'", shell=True)
 
 
-subprocess.call("root -l -b -q MassFit_FixPar_Data.C'(6.5, 9, 0, 1.6)'", shell=True)
-subprocess.call("root -l -b -q MassFit_FixPar_Data.C'(9, 12, 0, 1.6)'", shell=True)
-subprocess.call("root -l -b -q MassFit_FixPar_Data.C'(12, 15, 0, 1.6)'", shell=True)
-subprocess.call("root -l -b -q MassFit_FixPar_Data.C'(15, 20, 0, 1.6)'", shell=True)
-subprocess.call("root -l -b -q MassFit_FixPar_Data.C'(20, 25, 0, 1.6)'", shell=True)
-subprocess.call("root -l -b -q MassFit_FixPar_Data.C'(25, 50, 0, 1.6)'", shell=True)
+subprocess.call("root -l MassFit_FixPar_Data.C'(6.5, 9, 0, 1.6)'", shell=True)
+subprocess.call("root -l MassFit_FixPar_Data.C'(9, 12, 0, 1.6)'", shell=True)
+subprocess.call("root -l MassFit_FixPar_Data.C'(12, 15, 0, 1.6)'", shell=True)
 
-subprocess.call("root -l -b -q MassFit_FixPar_Data.C'(6.5, 50, 0, 1.6)'", shell=True)
+subprocess.call("root -l MassFit_FixPar_Data.C'(6.5, 50, 0, 1.6)'", shell=True)
 
 
-# ======================================================================= #
+#======================================================================= 
 
-
-subprocess.call("root -l -b -q Final2DFit.C'(3.5, 6.5, 1.6, 2.4)'", shell=True)
-subprocess.call("root -l -b -q Final2DFit.C'(6.5, 9, 1.6, 2.4)'", shell=True)
-subprocess.call("root -l -b -q Final2DFit.C'(9, 12, 1.6, 2.4)'", shell=True)
-subprocess.call("root -l -b -q Final2DFit.C'(12, 50, 1.6, 2.4)'", shell=True)
-
+#subprocess.call("root -l Final2DFit.C'(3.5, 6.5, 1.6, 2.4)'", shell=True)
+#subprocess.call("root -l Final2DFit.C'(6.5, 9, 1.6, 2.4)'", shell=True)
+subprocess.call("root -l Final2DFit.C'(12, 50, 1.6, 2.4)'", shell=True)
 subprocess.call("root -l -b -q Final2DFit.C'(3.5, 50, 1.6, 2.4)'", shell=True)
 
+subprocess.call("root -l -b Final2DFit.C'(6.5, 9, 0, 1.6)'", shell=True)
+subprocess.call("root -l -b Final2DFit.C'(9, 12, 0, 1.6)'", shell=True)
+subprocess.call("root -l -b Final2DFit.C'(12, 15, 0, 1.6)'", shell=True)
 
-subprocess.call("root -l -b -q Final2DFit.C'(6.5, 9, 0, 1.6)'", shell=True)
-subprocess.call("root -l -b -q Final2DFit.C'(9, 12, 0, 1.6)'", shell=True)
-subprocess.call("root -l -b -q Final2DFit.C'(12, 15, 0, 1.6)'", shell=True)
-subprocess.call("root -l -b -q Final2DFit.C'(15, 20, 0, 1.6)'", shell=True)
-subprocess.call("root -l -b -q Final2DFit.C'(20, 25, 0, 1.6)'", shell=True)
-subprocess.call("root -l -b -q Final2DFit.C'(25, 50, 0, 1.6)'", shell=True)
-
-subprocess.call("root -l -b -q Final2DFit.C'(6.5, 50, 0, 1.6)'", shell=True)
+subprocess.call("root -l -b Final2DFit.C'(6.5, 50, 0, 1.6)'", shell=True)
